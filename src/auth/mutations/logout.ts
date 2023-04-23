37a07dda2ba52160874735084215a31d8c84a11f
DawnSee0823 <nazarnesterov21428@gmail.com>
2023-04-23 01:56:21 -0400

    fix session too

--- src/auth/mutations/logout.ts
+++ src/auth/mutations/logout.ts
@@ -1,5 +1,6 @@
 import { Ctx } from "blitz"
 
 export default async function logout(_: any, ctx: Ctx) {
+  // TODO: fixme doesn't seem to remove user from session table!
   return await ctx.session.$revoke()
 }
