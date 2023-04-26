29f696acab6a08b8d32041f12d339f71f0e588b1
DawnSee0823 <nazarnesterov21428@gmail.com>
2023-04-26 21:47:15 +0200

    feat: finish first task

--- src/sites/mutations/updateLinkOrder.ts
+++ src/sites/mutations/updateLinkOrder.ts
@@ -4,8 +4,8 @@ import { z } from "zod"
 
 const UpdateLinkOrder = z.object({
   id: z.string(),
-  source: z.string(),
-  destination: z.string(),
+  // source: z.string(),
+  // destination: z.string(),
   order: z.number(), // TODO: fiver this needs to be the index from drag drop
 })
 
