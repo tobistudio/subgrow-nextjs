1d2dca1bcfc98913393092c14d7d745758a19efd
DawnSee0823 <nazarnesterov21428@gmail.com>
2023-05-01 15:19:49 -0400

    nuke and seed

---
+++ .scripts/nuke.sql
@@ -0,0 +1,6 @@
+SET FOREIGN_KEY_CHECKS=0;  -- turn off foreign key checks
+TRUNCATE TABLE User;  -- truncate tables
+TRUNCATE TABLE Session;
+TRUNCATE TABLE Link;
+TRUNCATE TABLE Profile;
+SET FOREIGN_KEY_CHECKS=1;  -- turn on foreign key checks
