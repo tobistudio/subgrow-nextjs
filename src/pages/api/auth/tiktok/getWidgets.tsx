ee77c25676c012dfce0c69a7fb757f453d223610
DawnSee0823 <nazarnesterov21428@gmail.com>
2023-05-09 22:00:02 +0200

    feat:update tiktok

--- src/pages/api/auth/tiktok/getWidgets.tsx
+++ src/pages/api/auth/tiktok/getWidgets.tsx
@@ -3,23 +3,21 @@
 export default async function handler(req, res) {
   try {
     // Fetch TikTok videos from trending endpoint
-    const response = await fetch('https://www.tiktok.com/node/share/discover?noUser=1&count=10');
+    const response = await fetch('https://www.tiktok.com/node/share/discover?noUser=1&count=30');
 
-    // if (!response.ok) {
-    //   throw new Error('Failed to fetch TikTok videos');
-    // }
+    if (!response.ok) {
+      throw new Error('Failed to fetch TikTok videos');
+    }
 
-    // const data = await response.json();
+    const data = await response.json();
 
-    // Randomly select a video
-    // const video = data.body[Math.floor(Math.random() * data.body.length)];
-    // const author = video.author.uniqueId.replace(/\s/g, '');
-    // const videoId = video.id;
+    const video = data.body[Math.floor(Math.random() * data.body.length)];
+    const author = video.author.uniqueId.replace(/\s/g, '');
+    const videoId = video.id;
 
-    // // Construct TikTok widget URL
-    // const url = `https://www.tiktok.com/@${author}/video/${videoId}`;
+    // Construct TikTok widget URL
+    const url = `https://www.tiktok.com/@${author}/video/${videoId}`;
 
-    // Send TikTok widget URL as response
     res.status(200).json({ response });
   } catch (error) {
     console.error(error);
