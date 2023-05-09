21707b0ca35b9a9305dd18dd7e558a8bea844185
DawnSee0823 <nazarnesterov21428@gmail.com>
2023-05-10 00:22:21 +0200

    feat: change style

--- src/profiles/layouts/TikTokWidget.tsx
+++ src/profiles/layouts/TikTokWidget.tsx
@@ -8,8 +8,10 @@ import {
     Typography,
     Button,
     Stack,
+    CardActionArea,
     Avatar
 } from "@mui/material"
+
 import axios from 'axios'
 
 export const TikTokWidget = () => {
@@ -30,23 +32,25 @@ export const TikTokWidget = () => {
                 tiktokWidget.map((ele, id) =>
                     id < 2 &&
                     <Card sx={{ maxWidth: 345 }} key={id}>
-                        <CardMedia
-                            sx={{ height: 140 }}
-                            image={ele.cardItem.cover}
-                            title="green iguana"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {ele.cardItem.title}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                {ele.cardItem.description}
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="small">Share</Button>
-                            <a href={`https://www.tiktok.com/${ele.cardItem.link}`}>Learn More</a>
-                        </CardActions>
+                        <CardActionArea>
+                            <CardMedia
+                                sx={{ height: 140 }}
+                                image={ele.cardItem.cover}
+                                title="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {ele.cardItem.title}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    {ele.cardItem.description}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Share</Button>
+                                <a href={`https://www.tiktok.com/${ele.cardItem.link}`}>Learn More</a>
+                            </CardActions>
+                        </CardActionArea>
                     </Card>
                 )
             }
